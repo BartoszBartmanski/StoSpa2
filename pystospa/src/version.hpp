8e950f26f941152766e9cffeb25da1ca51508d1b
bartosz.bartmanski <bartosz.bartmanski@maths.ox.ac.uk>
2020-01-28 22:11:02 +0100

    Updated setup.py and incremented build number

--- pystospa/src/version.hpp
+++
@@ -1,7 +0,0 @@
-
-#ifndef VERSION_HPP
-#define VERSION_HPP
-
-#define PROJECT_VERSION "2.0.16"
-
-#endif // VERSION_HPP
--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 
 from skbuild import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='pystospa',
-    version='2.0.16',
+    version='2.0.17',
     author='Bartosz Bartmanski',
     author_email='bartoszbartmanski@gmail.com',
     description='A python binding of C++ package for stochastic simulations of spatially extended systems',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     platforms=["Linux"],
     packages=['pystospa'],
     setup_requires=["setuptools", "wheel", "scikit-build >= 0.10.0", "cmake >= 3.5"],
