2616ecfa1fdb17c6f3d55380a4103be79a5e4341
bartosz.bartmanski <bartosz.bartmanski@maths.ox.ac.uk>
2020-03-05 11:29:14 +0100

    Added test for OSX to travis.yml

--- setup.py
+++ setup.py
@@ -11,12 +11,6 @@ with open("version", "r") as fh:
 with open(os.path.join("pystospa", "README.md"), "r") as fh:
     long_description = fh.read()
 
-# Add cmake arguments depending on the OS
-cmake_args = []
-mdt = os.getenv("MACOSX_DEPLOYMENT_TARGET")
-if mdt:
-    cmake_args.append("-DCMAKE_OSX_DEPLOYMENT_TARGET={}".format(mdt))
-
 # Run the installation
 setup(
     name='pystospa',
@@ -28,7 +22,6 @@ setup(
     long_description_content_type="text/markdown",
     platforms=["Linux"],
     packages=['pystospa'],
-    cmake_args = cmake_args,
     setup_requires=["setuptools", "wheel", "scikit-build >= 0.10.0", "cmake >= 3.5"],
     zip_safe=False,
 )
